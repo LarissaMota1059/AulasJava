54ca71b2a7b4f021d42dac3770ffffb72015f4cb
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-05-30 11:32:38 -0300

    Add files via upload

---
+++ Cadastro-Aluno/src/secre.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class secre {
+public static void main(String[] args) {
+	
+	Scanner secreta = new Scanner(System.in);
+	
+	String aluno[];
+	String endereco[];
+	String cpf[];
+	String nascimento[];
+	String responsa [];
+	
+	
+	aluno = new String[10];
+	endereco = new String [10];
+	cpf =  new String [10];
+	nascimento =  new String [10];
+	responsa = new String [10];
+	
+        System.out.print("Nome do Aluno: ");
+		secreta.nextLine();
+		
+        System.out.print("Endereço do aluno: ");		
+		 secreta.nextLine();
+		
+	
+		
+		
+		
+	secreta.close();
+	
+	
+}
+}
