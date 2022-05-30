136aa4fd76b3ae742c8955003b41036ef2be7a66
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-05-30 11:32:06 -0300

    Add files via upload

---
+++ Cadastro/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		List <Pessoa> listaPessoas = new ArrayList<Pessoa>();
+		
+		Scanner teclado =  new Scanner (System.in);
+		Pessoa pessoa = new Pessoa();
+		
+		Sexo sexo = Sexo.MASCULINO;
+
+		
+		pessoa.setEndereco(new Endereco());
+		
+		for(int  x=0;  x <4 ; x++ ) {
+		
+		System.out.println("Digite seu nome: ");
+		pessoa.setNome(teclado.next());
+		
+		System.out.println("Digite sua idade: ");
+		pessoa.setIdade(teclado.nextInt());
+		
+		System.out.println("Digite a rua do seu endereço");
+		pessoa.getEndereco().setRua(teclado.next());
+		
+		
+		System.out.println("Digite o numero do seu endereço");
+		pessoa.getEndereco().setNumero(teclado.nextInt());
+		
+		System.out.println("Digite o bairro do seu endereço");
+		pessoa.getEndereco().setBairro(teclado.next());
+		
+		System.out.println("Digite 1 para o sexo Masculino e Digite 2 para o sexo feminino");
+		
+		
+		if(teclado.nextInt() == 1) {
+			 sexo = Sexo.MASCULINO;
+		}
+		else {
+			 sexo = Sexo.FEMININO;
+		}
+		
+		listaPessoas.add(pessoa);
+		}
+		System.out.println(listaPessoas);
+	}
+}
---
+++ Cadastro/src/Pessoa.java
@@ -0,0 +1,57 @@
+
+public class Pessoa {
+	
+	private String nome;
+	private int idade;
+	private	Endereco endereco;
+	private Sexo sexo;
+	
+	
+	
+	// <------------------------------Getter e Setter--------------------------------------->
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	
+	// <------------------------------Getter e Setter--------------------------------------->
+	
+	
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome = " + nome + ", Idade = " + idade + ", endereco = "+ endereco + " ,sexo = " + sexo +"]";
+	}
+
+}
