20e0acccd90a6130421733fec76c212009001188
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-05-30 11:33:28 -0300

    Add files via upload

---
+++ ElementosEstaticos/src/Aplicacao.java
@@ -0,0 +1,44 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+Matematica matematica = new Matematica();    //criaçao de um novo objeto
+		
+//		int resultado = matematica.somar(2,3);  //criaçao de uma nova variavel, mostrando que recebe  um valor de um objeto junto com metodo somar, dado parametros
+//		System.out.println((resultado)); // mostra o resultado na tela
+//		resultado = matematica.subtrair(10, 4); //a variavel resultado recebe o valor do objeto com o metodo subtrair, dado parametros
+//		System.out.println(resultado);
+		
+		
+//		int resultado = Matematica.somar(2, 4) ;
+//		System.out.println(resultado);
+//		
+//		resultado = Matematica.subtrair(2, 4) ;
+//		System.out.println(resultado);
+		
+//		Contador contador1 = new Contador();  //criaçao de um novo objeto
+//		
+//		Contador contador2 = new Contador();  //criaçao de um novo objeto
+//		
+//		Contador contador3 = new Contador();  //criaçao de um novo objeto
+//		
+//		static contador1.incrementar();
+//		
+//		System.out.println(contador1.getValor());
+//		System.out.println(contador2.getValor());
+//		System.out.println(contador3.getValor());
+//		
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		System.out.println(Contador.getValor());
+//		
+//	}
+
+		System.out.println(Constantes.MEDIA_DA_PROVA);
+	}
+
+	}
+
+
---
+++ ElementosEstaticos/src/Contador.java
@@ -0,0 +1,33 @@
+
+public class Contador {
+	
+private static int valor; //criacao de uma variavel do tipo inteiro (int)
+	
+	
+	
+//	public Contador() {  
+//		this.valor = 1;
+//	}
+	static{  //informaçao fica estatica, e dá para ser usado para implementaçao de valores futuros
+		valor = 1;
+	}
+	
+
+	public static void incrementar() {  //incrementa um valor na variavel
+		valor ++;
+	}
+	
+	
+	public static int getValor() { //pega o valor da variavel e retorna ele mesmo
+		return valor;
+	}
+
+	public void setValor(int valor) {  //modifica o valor da variavel,dentro de uma nova variavel que poderá ser usada apenas nesse bloco de codigo
+		this.valor = valor;
+	}
+	
+	
+	
+	
+
+}
