88e810961532631782cc4ffb1cec7bec21150356
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:56:24 -0300

    Update Venda.java

--- Loja/src/Venda.java
+++ Loja/src/Venda.java
@@ -2,15 +2,38 @@
 public class Venda {
 
 	public static void main(String[] args) {
-		double prod1 = 20;
-		double prod2 = 28;
-		double total;
-		double desc; 
-		
-		total = prod1 + prod2;
-		desc = total * 0.85;
-		System.out.println("total a pagar com desconto é: " + desc);                                                                                                                                      
-	
+		//  instancia a variavel valor do tipo Scanner. A classe Scanner serve para permitir que o usuario possa digitar com o console
+				Scanner valor = new Scanner(System.in);
+				
+				// Criacao dos atributos da classe
+				String p1;
+				String p2;
+				double v1;
+				double v2;
+				double total;
+				double desconto;
+				
+				// Interagindo com o usuario por meio da classe scanner
+				System.out.println("Escolha o produto desejado ");
+				p1 = valor.next();
+				
+				System.out.println("Escolha o produto desejado ");
+				p2 = valor.next();
+				
+				System.out.println("Digite o valor do produto 1 ");
+				v1 = valor.nextDouble();
+				
+				System.out.println("Digite o valor do produto 2 ");
+				v2 = valor.nextDouble();
+				
+				// Calculo do valor que o usuario inseriu
+				total=v1+v2;
+				// Calculo de desconto de 15%
+				desconto=total*0.85;
+				
+				// Mensagem Final
+				System.out.println("O valor total dos produtos com o desconto de 15% Ã©: " + desconto);
+			}
 		
 		
 
