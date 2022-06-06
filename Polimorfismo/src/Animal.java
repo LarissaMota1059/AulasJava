af9ad5676c93e21c1a2c9b52ad3f6331d058b7a8
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:11:36 -0300

    Update Animal.java

--- Polimorfismo/src/Animal.java
+++ Polimorfismo/src/Animal.java
@@ -1,6 +1,6 @@
 
-public class Animal {
+public class Animal {  //essa classe é tipo uma super que tem como herdar caracteristicas dela
 		
-	public  void  falar() {
+	public  void  falar() {  //metodo generio "falar"
 }
 }
