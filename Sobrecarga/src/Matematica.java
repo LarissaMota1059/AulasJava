1ee6b6d886c7449158fae8bff330061b759fc289
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 08:58:49 -0300

    Update Matematica.java

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -1,21 +1,17 @@
 
 public class Matematica {
 
- int somar (int x, int y) {
-		
-//		int z =  x + y;
-	
-//		return z;
-		return  x + y;
-	}
-	
-	int somar(int x, int y, int z) {
-		
-		return  x + y + z;
-	}
-	
-	double somar (double x, double y) {
-		
-		return x + y;
-	}
-}
+ double area(double x, double y) {  // metodo chamado area, ele é um double que ira calcular a area de um triangulo 
+	 return x * y / 2;
+	 System.out.println("A área do seu triangulo é:" + area);
+ }
+ 
+ int area(int x, int y) {  //metodo area, que calcula a area de um quadrado e de um triangulo se o valor de x e y forem iguais 
+	 //ao inves de ser double é int
+ }
+	 if (x == y) {
+		 return x * y;
+	 }else {
+		 return x * y;
+	 }
+ }
