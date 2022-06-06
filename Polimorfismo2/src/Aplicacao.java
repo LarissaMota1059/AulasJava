34542783d0b460d2cf0fef58da9ee0415acfa8df
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:00:05 -0300

    Update Aplicacao.java

--- Polimorfismo2/src/Aplicacao.java
+++ Polimorfismo2/src/Aplicacao.java
@@ -3,13 +3,13 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Preparador preparador = new Preparador(); //Método
+		PPreparador preparador = new Preparador(); //indtanciando a classe preparador pela variavel preparador
 		
-		LeiteQuente leiteQuente = new LeiteQuente();
-		preparador.prepararBebida(leiteQuente);
+		LeiteQuente leiteQuente = new LeiteQuente();  // objeto leiteQuente do tipo leiteQuente
+		preparador.prepararBebida(leiteQuente);  //metodod preparar bebeida e passando como parametro o leiteQuente
 		
-		Refrigerante refrigerante = new Refrigerante();
-		preparador.prepararBebida(refrigerante);
+		Refrigerante refrigerante = new Refrigerante();  //objeto refrigerante do tipo refrigerante
+		preparador.prepararBebida(refrigerante);  //metodo preparar bebida e passando como parametro o refrigerante
 
 	}
 
