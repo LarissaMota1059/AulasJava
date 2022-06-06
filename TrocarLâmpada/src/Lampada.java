2cbc4c95e64f66ccfcbaa3018287dc2208a460fc
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 08:54:06 -0300

    Update Lampada.java

--- TrocarLâmpada/src/Lampada.java
+++ TrocarLâmpada/src/Lampada.java
@@ -3,7 +3,20 @@ public class Lampada {
 
 	public static void main(String[] args) {
 
-	System.out.println("Hello Word! Ol� mundo!"); 
+	//Projeto para testar o comando System.out.println
+		System.out.println("Hello World, Olá Mundo, Hola Mundo");
+		System.out.println("----------------------------------");
+		System.out.println("Pegar a lâmpada");
+		System.out.println("Pegar a escada");
+		System.out.println("Deixar a lâmpada em algum lugar próximo da que será trocada");
+		System.out.println("Colocar a escada em baixo da lâmpada quebrada");
+		System.out.println("Remover a lâmpada quebrada");
+		System.out.println("Colocar em algum local seguro");
+		System.out.println("Pegar a lâmpada nova");
+		System.out.println("Subir na escada novamente");
+		System.out.println("Colocar a lâmpada nova");
+		System.out.println("Descer da escada");
+		System.out.println("Testar a lâmpada");
 		
 		
 		
