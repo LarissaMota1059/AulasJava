1fe97b2e92e6031358833b25de0eaf0cc51d9d6d
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:46:52 -0300

    Update Main.java

--- OrientaçãoObjeto/src/trocaFigurinha/Main.java
+++ OrientaçãoObjeto/src/trocaFigurinha/Main.java
@@ -5,36 +5,31 @@
 
 			public static void main(String[] args) {
 				
-				Pessoa pessoa1 =  new Pessoa();
 				
-				pessoa1.nome = "Larissa";
-				pessoa1.numFigurinhas = 1;
-				
-				pessoa1.Receber(3);
-				
-				System.out.println(pessoa1.nome);
-				System.out.println(pessoa1.numFigurinhas);
-				
-				Pessoa pessoa2 = new Pessoa();
-				
-				pessoa2.nome = "Carla";
-				pessoa2.numFigurinhas = 5;
-				
-				System.out.println(pessoa2.nome);
-				System.out.println(pessoa2.numFigurinhas);
-				
-				boolean msg = pessoa1.Dar(2, pessoa2);
-				
-				if(msg==true) {
-				System.out.println(pessoa1.nome);
-				System.out.println(pessoa1.numFigurinhas);
-				
-				System.out.println(pessoa2.nome);
-				System.out.println(pessoa2.numFigurinhas);
-				}
-				
-				
-			}
+		Pessoa pessoa1 = new Pessoa(); //perfil Larissa
+		pessoa1.nome = "Larissa";
+		pessoa1.numFigurinhas = 20;
+		pessoa1.x = 1;
+		System.out.println("Número de figurinhas iniciais do Larissa: " + pessoa1.numFigurinhas);
+
+		
+		Pessoa pessoa2 = new Pessoa();  //perfil Carla
+		pessoa2.nome = "Carla";
+		pessoa2.numFigurinhas = 20;
+		System.out.println("Número de figurinhas iniciais do carla: " + pessoa2.numFigurinhas + "\n");
+
+		// Realizando a troca
+		pessoa1.Dar(35, pessoa2);
+
+		// Resultado final
+		// Condição para exibir a mensagem do resultado apenas se a variavel x for igual a 1 conforme indicado na Classe Pessoa
+		if (pessoa1.x > 1) {
+			System.out.println("");
+		} else {
+			System.out.println("Número de figurinhas do Pedro depois da troca: " + pessoa1.numFigurinhas);
+			System.out.println("Número de figurinhas do Jorge depois da troca: " + pessoa2.numFigurinhas);
+		}
 	}
+}
 
 
