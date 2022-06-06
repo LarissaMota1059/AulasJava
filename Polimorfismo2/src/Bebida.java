7941891320eaafbd0826979d4c318d0949f408c5
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:02:23 -0300

    Update Bebida.java

--- Polimorfismo2/src/Bebida.java
+++ Polimorfismo2/src/Bebida.java
@@ -1,22 +1,23 @@
 
 public class Bebida { //Super classe- Bebida
 	
-	private String nome; //atributo
-	private boolean aquecer; //atributo
+	private String nome; //criando atributo de beiba
+	private boolean aquecer; 
 	
 	
-	public Bebida(String nome, boolean aquecer) {
+	public Bebida(String nome, boolean aquecer) {  //metodo condutor da classe bebida
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 	}
 	
-	public void preparar() { //Método- preparar
+	public void preparar() { //metodo parar qualquer tipo de bebida
 		System.out.println("Pegar copo");
 	}
 
 
-	public String getNome() { 
+	public String getNome() { //getters e setters dos atributos dessa classe 
+		//get pega o valor e set atribue um valor
 		return nome;
 	}
 
