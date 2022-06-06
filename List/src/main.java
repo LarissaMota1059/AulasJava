e95f06deef8e7a9ca0c942e8965076a5ebe5670b
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:58:34 -0300

    Update main.java

--- List/src/main.java
+++ List/src/main.java
@@ -7,16 +7,68 @@ public class main {
 		
 		List lista = new ArrayList();
 		
-		lista.add("abc");
+		List lista = new ArrayList(); //Instanciar uma lista
+		
+		lista.add("abc"); //Como a list vem da object, o polimorfismo dela permite colocar vários valores de difentes tipos
 		lista.add(12);
 		lista.add(true);
-		for(int x=0; x< lista.size(); x++) {
-			System.out.println(lista.get(0));
+		
+		//Aqui  o for foi usado para interagir com a lista e permitir que a variável x mostrasse os valores da lista (Interação mais trabalhosa)
+		for(int x=0;x<lista.size();x++) {
+		System.out.println(lista.get(x));
+		
 		}
-		for(Object valor : lista) {
+		
+
+		//Enhanced for facilita o código, pois automaticamente interage com a lista (Interação mais simples)
+		for(Object valor: lista) {
 			System.out.println(valor);
 		}
+		System.out.println();
+	
+		//No remove pode-se utilizar tanto o index como o objeto
+		//Ao contrário do array, aqui quando você exclui um item ele não mostra nulo, mas remove completamente, pois utiliza o método lista e não direto o objeto
+		lista.remove(0);
+		for(Object valor: lista) {
+			System.out.println(valor);
+		}
+		System.out.println();
+		
+		//O add empurra a valor que está na posição 0
+		lista.add(0, "hue");
+		for(Object valor: lista) {
+			System.out.println(valor);
+		}
+		System.out.println();
+	
+		
+		lista.remove(0);
+		for(Object valor: lista) {
+			System.out.println(valor);
+		}
+		System.out.println();
+		
+		
+		lista.set(0, "hue");//Set substitui o que estava na posição 0
+		for(Object valor: lista) {
+			System.out.println(valor);
+		}
+		System.out.println();
+		
+		
+		lista.clear();  //O comando clear limpa os valores da lista
+		if(lista.isEmpty()) {
+			System.out.println("Sua lista está vazia");
+		}
 
+		Conta conta1 = new Conta("Água",60.00);
+		Conta conta2 = new Conta("Luz",120.00);
+		
+		List<Conta>listaContas = new ArrayList<Conta>();
+		listaContas.add(conta1);
+		listaContas.add(conta2);
+		
+		for(Conta conta:listaContas) {
+			System.out.println(conta);
+		}
 	}
-
-}
