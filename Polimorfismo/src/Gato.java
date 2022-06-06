decfb87512c458b3c189de697a2df890d5ce3750
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 09:36:41 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -2,7 +2,7 @@
 public class Gato extends  Animal { 
 	
 	@Override
-	public  void  falar() {
+	public  void  falar() {  //metodo falar do gato tem suas proprias caracteristicas que é diferente do cachorro
 		
 		System.out.println("MIAU");
 	}
