b858d63d1f36603dc7b526a2f61e04a0586014c0
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-06 10:11:21 -0300

    Update Caminhao.java

--- Heranca/src/Caminhao.java
+++ Heranca/src/Caminhao.java
@@ -2,7 +2,7 @@
 public class Caminhao extends Veiculo {
 
 	@Override
-	public void buzinar() {
+	public void buzinar() {  //sobcrito metodo buzinar, por a buzina do caminhao é diferente
 		System.out.println("FOOOOM");
 	}
 
