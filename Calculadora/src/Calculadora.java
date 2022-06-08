9c84a4ef9eda478741cc34f83d81199ff6ac3230
LarissaMota1059 <103435612+LarissaMota1059@users.noreply.github.com>
2022-06-08 10:22:19 -0300

    Add files via upload

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,30 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		// Declarando variaveis
+				double n1=10;
+				double n2=3;
+				double resultSoma;
+				double resultSub;
+				double resultMult;
+				double resultDiv;
+				
+				// Atribuindo contas as variaveis criadas
+				resultSoma = n1+n2;
+				System.out.println("A soma é: " + resultSoma);
+				
+				resultSub = n1-n2;
+				System.out.println("A subtração é: " + resultSub);
+				
+				resultMult = n1*n2;
+				System.out.println("A multiplicação é: " + resultMult);
+				
+				resultDiv = n1/n2;
+				System.out.println("A divisão é: " + resultDiv);
+				
+			}
+
+	}
+
